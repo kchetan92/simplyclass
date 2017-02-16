5e458e836477e7ea4690eba5fbf63ccd56c32b35
chetan <chetan.k@via.com>
2017-02-16 22:50:11 +0530

    suggestions added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,2 +1,9 @@
 $(document).foundation();
-console.log('Like what you see here? Contact Chetan : kchetan.com, k.chetan92@gmail.com')
+console.log('Like what you see here? Contact Chetan : kchetan.com, k.chetan92@gmail.com');
+$(document).imagesLoaded()
+	.done(function(){
+		var hash = window.location.hash;
+		if(hash) {
+			window.location = hash;
+		}
+	});
